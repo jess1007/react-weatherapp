984f0507f72aa1c318e45ef6327f8a2853b6b29b
jess1007 <jessica.gernelle@gmail.com>
2023-12-30 21:27:48 +1100

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <footer>
+        This{" "}
+        <a
+          href="https://github.com/jess1007/react-weatherapp"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          project
+        </a>{" "}
+        was coded by Jess Gernelle
+      </footer>
     </div>
   );
 }
